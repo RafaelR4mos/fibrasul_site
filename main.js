67cfa8e69845d11c1a6a53be2ffab9c82db9f14a
Rafael <rafaelramos0401@gmail.com>
2022-11-28 16:50:54 -0300

    Prepared the cards-section to work on small devices without breaking content

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-import './styles/index.css'
+import "./styles/index.css";
 
 const imagesAmount = document.querySelectorAll(".carousel-content").length;
-const rightCarouselArrow = document.getElementById('btnR');
-const leftCarouselArrow = document.getElementById('btnL');
-const carouselBulletNav = document.querySelector('.carousel-bullet-nav-container');
-const bulletPoints = document.querySelector('.bullet-nav');
-
+const rightCarouselArrow = document.getElementById("btnR");
+const leftCarouselArrow = document.getElementById("btnL");
+const indicatorParents = document.querySelector(".carousel-bullet-nav-container");
+const carouselSwipeArea = document.querySelector("#carousel-main");
 
 let images;
 let imagesPosition = 0;
@@ -14,57 +13,82 @@ let resizeTimer;
 var arrayPosition = 0;
 
 
-carouselBulletNav.addEventListener('click', (e) => {
-  const clickedElement = e.target
-
-  if (clickedElement.id == 'bullet-1') {
-    move(-1);
-  }
-  else if (clickedElement.id == 'bullet-2') {
-    move(-2);
-  }
-  else if (clickedElement.id == 'bullet-3') {
-    move(-3);
-  }
+document.querySelectorAll("#carousel-section .opa").forEach(function(indicator, ind) {
+    indicator.addEventListener("click", function() {
+    arrayPosition = ind;
+    indicator.classList.add("selected");
 })
+});
 
 
 // USED TO CONTROL ARRAY POSITION VALUES
-rightCarouselArrow.addEventListener('click', () => {
-  arrayPosition += 1;
-})
+leftCarouselArrow.addEventListener("click", function() {
+  arrayPosition = (arrayPosition > 0) ? arrayPosition - 1 : 0;
+  setIndex();
+  indicatorParents.children[arrayPosition].classList.add("selected");
+  console.log(arrayPosition)
+});
 
-leftCarouselArrow.addEventListener('click', () => {
-  arrayPosition -= 1;
-  
-console.log(arrayPosition)
-})
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.documentElement.style.setProperty('--windowWidth', `${(window.innerWidth / 2000) + "s"}`);
-
-  images = document.querySelector('#carousel-items')
-
-  document.querySelector('#btnL').addEventListener("click", () => { move(1) })
-  document.querySelector('#btnR').addEventListener("click", () => { move(-1) })
-  document.querySelector('#carousel-main').addEventListener("touchstart", (event) => { touchStart = Math.floor(event.changedTouches[0].screenX) })
-  document.querySelector('#carousel-main').addEventListener("touchend", (event) => {
-      touchEnd = Math.floor(event.changedTouches[0].screenX);
-      if (touchStart > touchEnd) {
-          move(-1);
-      } else {
-          move(1);
-      }
-  });
+rightCarouselArrow.addEventListener("click", function() {
+  arrayPosition = (arrayPosition < 2) ? arrayPosition + 1 : 2;
+  setIndex();
+  indicatorParents.children[arrayPosition].classList.add("selected");
+  console.log(arrayPosition)
 });
 
 
+
+function setIndex() {
+  document.querySelector('#carousel-section .selected').classList.remove("selected");
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.documentElement.style.setProperty(
+    "--windowWidth",
+    `${window.innerWidth / 2000 + "s"}`
+  );
+
+  images = document.querySelector("#carousel-items");
+
+  document.querySelector("#btnL").addEventListener("click", () => {
+    move(1);
+  });
+  document.querySelector("#btnR").addEventListener("click", () => {
+    move(-1);
+  });
+  carouselSwipeArea
+    .addEventListener("touchstart", (event) => {
+      touchStart = Math.floor(event.changedTouches[0].screenX);
+    });
+    carouselSwipeArea
+    .addEventListener("touchend", (event) => {
+      touchEnd = Math.floor(event.changedTouches[0].screenX);
+      if (touchStart > touchEnd) {
+        move(-1);
+        arrayPosition = (arrayPosition < 2) ? arrayPosition + 1 : 2;
+        setIndex();
+        indicatorParents.children[arrayPosition].classList.add("selected");
+        console.log(arrayPosition)
+      } else {
+        move(1);
+        arrayPosition = (arrayPosition > 0) ? arrayPosition - 1 : 0;
+        setIndex();
+        indicatorParents.children[arrayPosition].classList.add("selected");
+      }
+    });
+});
+
 window.addEventListener("resize", function () {
-  document.documentElement.style.setProperty('--windowWidth', `${(window.innerWidth / 2000) + "s"}`);
+  document.documentElement.style.setProperty(
+    "--windowWidth",
+    `${window.innerWidth / 2000 + "s"}`
+  );
   move(0);
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   document.body.classList.add("resize-animation-stopper");
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(() => {
@@ -73,19 +97,22 @@ window.addEventListener('resize', () => {
 });
 
 
-
 function move(direction) {
   const imageW = images.offsetWidth;
   imagesPosition += direction;
 
-  imagesPosition = (imagesPosition > 0) ? 0 : (Math.abs(imagesPosition) <= (imagesAmount - 1)) ? imagesPosition : -(imagesAmount - 1);
+  imagesPosition =
+    imagesPosition > 0
+      ? 0
+      : Math.abs(imagesPosition) <= imagesAmount - 1
+      ? imagesPosition
+      : -(imagesAmount - 1);
 
   if (direction == 0) {
-      images.classList.remove('animate');
+    images.classList.remove("animate");
   } else {
-      images.classList.add('animate');
+    images.classList.add("animate");
   }
 
   images.style.transform = "translateX(" + imagesPosition * imageW + "px)";
 }
-
